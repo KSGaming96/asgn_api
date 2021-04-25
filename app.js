ab6486256b4ce068d8edd40d3bf98a1fbcef7433
KSGaming96 <ksgaming96@gmail.com>
2021-04-25 13:49:01 -0600

    Initial Set Up

---
+++ app.js
@@ -0,0 +1,30 @@
+let express = require('express')
+let app = express();
+const apiRouter = require('./api-router.js')
+let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+
+//Set up mongoose and body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+//connect to mongoose
+mongoose.connect("mongodb://localhost/asgn_api", { useNewUrlParser: true});
+var db = mongoose.connection;
+
+if (!db) {
+    console.log("Error connecting to the DB.");
+} else {
+    console.log("DB Connected Sucessfully.");
+}
+
+var port = 8080;
+
+app.use("/api", apiRouter);
+
+app.get('/', (req, res) => res.send('Hello World with Express'));
+
+app.listen(port, function () {
+    console.log("Running contacts-api on port " + port);
+});
